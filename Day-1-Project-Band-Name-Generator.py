2e4356978309d389afbe2dcd47d9625668c3f31e
Melvin Sewak <melvinsewak@outlook.com>
2021-11-10 22:33:14 -0500

    Day 1

--- Day-1-Project-Band-Name-Generator.py
+++ Day-1-Project-Band-Name-Generator.py
@@ -1,1 +1,4 @@
-print("test")
+print("Welcome to Band Name Generator")
+city = input("Enter the city in which you were born: ")
+pet = input("Enter the name of your pet: ")
+print("Your band name could be '" + city +" " + pet+"'")
